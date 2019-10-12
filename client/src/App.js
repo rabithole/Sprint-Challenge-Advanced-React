795ecb9a8058e459eacfe2e66214f74514e87b85
Robert Kulp <pedalinon@gmail.com>
2019-10-12 07:26:54 -0700

    Still working on passing props

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import Player from './Player';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/players')
+      .then(res => {
+        this.setState({ 
+          name: res.data
+        })
+
+        console.log(this.state.name);
+        console.log(res.data[0].name);
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Women's World Cup Roster</h1>
+        </header>
+        <Player 
+          players={this.state.players}
+        />
     </div>
-  );
+    )
+  }
 }
 
 export default App;
---
+++ client/src/Player.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Player = props => {
+  console.log(props);
+  return (
+    <div className=''>
+      <h2>Players</h2>
+      <div>
+
+      </div>
+    </div>
+  )
+};
+
+export default Player; 
